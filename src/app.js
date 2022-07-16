8bfcefcec393ea2304e43841037d8f5cdd4d8773
Alisa Velyka <sorrybabyxxwh@gmail.com>
2022-07-16 14:44:59 +0300

    added icon

--- src/app.js
+++ src/app.js
@@ -60,6 +60,9 @@ function showTemperature(response) {
     windElement.innerHTML = `${wind} km/h`
     let currentDateElement = document.getElementById("date");
     currentDateElement.innerHTML = formattedDate(response.data.dt * 1000);
+    let iconElement = document.getElementById("icon");
+    iconElement.setAttribute("alt",response.data.weather[0].description);
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 function displayCityName(response){
